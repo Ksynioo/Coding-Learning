bfc1c732b14cc86655466fd5adcf9e4a6b5ec921
Ksynioo <krzstr98@gmail.com>
2024-09-30 11:14:30 +0200

    New CSS & C++ Tasks
    Created CSS text formatting style
    
    Created C++ Hello world program once again in Visual Studio

---
+++ [C++]/HelloWorldOnceAgain - Visual Studio/Source.cpp
@@ -0,0 +1,14 @@
+// Project Hello World once again
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World Once Again!" << endl;
+	system("pause");
+	getch();
+	return 0;
+}
