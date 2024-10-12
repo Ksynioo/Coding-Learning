5d5ee98715edf4e72ddcb145a35ab01e9069254c
Ksynioo <krzstr98@gmail.com>
2024-10-12 12:53:16 +0200

    New CSS style & C++ Project
    Created CSS Combinators style
    
    Created C++ Calculator project

---
+++ [C++]/[PROJECTS]/Calculator - Visual Studio/CalcForm.h
@@ -0,0 +1,73 @@
+#pragma once
+
+namespace Calculator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for CalcForm
+	/// </summary>
+	public ref class CalcForm : public System::Windows::Forms::Form
+	{
+	public:
+		CalcForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~CalcForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(CalcForm::typeid));
+			this->SuspendLayout();
+			// 
+			// CalcForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Black;
+			this->ClientSize = System::Drawing::Size(634, 411);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MaximizeBox = false;
+			this->Name = L"CalcForm";
+			this->Text = L"Calculator";
+			this->Load += gcnew System::EventHandler(this, &CalcForm::CalcForm_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void CalcForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
