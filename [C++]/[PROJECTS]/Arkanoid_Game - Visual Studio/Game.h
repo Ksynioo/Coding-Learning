c4cdc6353cc4e17c53f507aa686a85094ba2a0b8
Ksynioo <krzstr98@gmail.com>
2024-10-30 16:20:11 +0100

    Arkanoid C++ Game update
    Created ball image and animation

--- [C++]/[PROJECTS]/Arkanoid_Game - Visual Studio/Game.h
+++ [C++]/[PROJECTS]/Arkanoid_Game - Visual Studio/Game.h
@@ -34,12 +34,16 @@ namespace ArkanoidGame {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ ball;
+	protected:
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +52,73 @@ namespace ArkanoidGame {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Game";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Game::typeid));
+			this->ball = (gcnew System::Windows::Forms::PictureBox());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// ball
+			// 
+			this->ball->BackColor = System::Drawing::Color::Transparent;
+			this->ball->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->ball->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ball.Image")));
+			this->ball->Location = System::Drawing::Point(336, 329);
+			this->ball->Name = L"ball";
+			this->ball->Size = System::Drawing::Size(25, 25);
+			this->ball->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->ball->TabIndex = 0;
+			this->ball->TabStop = false;
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 1;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Game::timer1_Tick);
+			// 
+			// Game
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(704, 501);
+			this->Controls->Add(this->ball);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"Game";
+			this->Text = L"Arkanoid";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
+
+		int x = 5, y = 5;
 #pragma endregion
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		//ball movement
+		ball->Left += x;
+		ball->Top += y;
+
+		if (ball->Left >= Game::Width - ball->Width)
+		{
+			x = -x;
+		}
+		if (ball->Top >= Game::Height - ball->Height)
+		{
+			y = -y;
+		}
+		if (ball->Top <= 75)
+		{
+			y = -y;
+		}
+		if (ball->Left <= 5)
+		{
+			x = -x;
+		}
+
+	}
 	};
 }
