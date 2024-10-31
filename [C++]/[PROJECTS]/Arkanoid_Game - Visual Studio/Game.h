2fee81ac8deef73d8910d733a8ffe2fb167a1ad8
Ksynioo <krzstr98@gmail.com>
2024-10-31 17:44:29 +0100

    Arkanoid game update
    Created platform and created movement for platform

--- [C++]/[PROJECTS]/Arkanoid_Game - Visual Studio/Game.h
+++ [C++]/[PROJECTS]/Arkanoid_Game - Visual Studio/Game.h
@@ -37,6 +37,7 @@ namespace ArkanoidGame {
 	private: System::Windows::Forms::PictureBox^ ball;
 	protected:
 	private: System::Windows::Forms::Timer^ timer1;
+	private: System::Windows::Forms::PictureBox^ platform;
 	private: System::ComponentModel::IContainer^ components;
 
 	private:
@@ -56,7 +57,9 @@ namespace ArkanoidGame {
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Game::typeid));
 			this->ball = (gcnew System::Windows::Forms::PictureBox());
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->platform = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->platform))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// ball
@@ -64,7 +67,7 @@ namespace ArkanoidGame {
 			this->ball->BackColor = System::Drawing::Color::Transparent;
 			this->ball->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->ball->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ball.Image")));
-			this->ball->Location = System::Drawing::Point(336, 329);
+			this->ball->Location = System::Drawing::Point(319, 430);
 			this->ball->Name = L"ball";
 			this->ball->Size = System::Drawing::Size(25, 25);
 			this->ball->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
@@ -77,6 +80,17 @@ namespace ArkanoidGame {
 			this->timer1->Interval = 1;
 			this->timer1->Tick += gcnew System::EventHandler(this, &Game::timer1_Tick);
 			// 
+			// platform
+			// 
+			this->platform->BackColor = System::Drawing::Color::Transparent;
+			this->platform->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"platform.Image")));
+			this->platform->Location = System::Drawing::Point(266, 461);
+			this->platform->Name = L"platform";
+			this->platform->Size = System::Drawing::Size(133, 28);
+			this->platform->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->platform->TabIndex = 1;
+			this->platform->TabStop = false;
+			// 
 			// Game
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
@@ -84,41 +98,62 @@ namespace ArkanoidGame {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(704, 501);
+			this->Controls->Add(this->platform);
 			this->Controls->Add(this->ball);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->MaximizeBox = false;
 			this->Name = L"Game";
 			this->Text = L"Arkanoid";
+			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &Game::movePlatform);
+			this->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &Game::Game_KeyUp);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->platform))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 
-		int x = 5, y = 5;
+		int x = 0, y = 0;
+		char direction;
 #pragma endregion
 	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+
 		//ball movement
 		ball->Left += x;
 		ball->Top += y;
 
-		if (ball->Left >= Game::Width - ball->Width)
+		if (ball->Left >= Game::Width - ball->Width) x = -x;
+		if (ball->Top >= Game::Height - ball->Height) y = -y;
+		if (ball->Top <= 75) y = -y;
+		if (ball->Left <= 5) x = -x;
+
+
+		//platform movement right
+		if (direction == 'R' && (platform->Left<Game::Width - platform->Width - 30))
 		{
-			x = -x;
+			platform->Left += 20;
 		}
-		if (ball->Top >= Game::Height - ball->Height)
+
+		//platform movement left
+		if (direction == 'L' && (platform->Left > 10))
 		{
-			y = -y;
-		}
-		if (ball->Top <= 75)
-		{
-			y = -y;
-		}
-		if (ball->Left <= 5)
-		{
-			x = -x;
+			platform->Left -= 20;
+
 		}
 
 	}
-	};
+	private: System::Void movePlatform(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+		 
+		if (e->KeyCode == Keys::Left) direction = 'L';
+		if (e->KeyCode == Keys::Right) direction = 'R';
+		if (e->KeyCode == Keys::Space)
+		{
+			x = -5;
+			y = -5;
+		}
+	}													 
+private: System::Void Game_KeyUp(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	direction = 'S';
+}
+};
 }
